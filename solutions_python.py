352684796f407f46377b4c7bd2a5bbf5f731d0c7
Gabriela <gabrielaa.bdasilva@gmail.com>
2023-06-19 14:42:41 -0700

    Mock Interview

--- solutions_python.py
+++ solutions_python.py
@@ -3234,4 +3234,101 @@ print(count_islands(grid2))
 
     
     
+# bugged fixed 
 
+# Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+# An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+# Example 1:
+
+# Input: grid = [
+#   ["1","1","1","1","0"],
+#   ["1","1","0","1","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","0","0","0"]
+# ]
+# Output: 1
+# Example 2:
+
+# Input: grid = [
+#   ["1","1","0","0","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","1","0","0"],
+#   ["0","0","0","1","1"]
+# ]
+# Output: 3
+
+# Input: grid = [
+#   ["0","0","0","0","0"],
+#   ["0","0","0","0","0"],
+#   ["0","0","1","0","0"],
+#   ["0","0","0","0","0"]
+# ]
+# Output: 1
+
+# row = i
+# column = j
+
+
+class State:
+  UNVISITED = 0
+  VISITING = 1
+  VISITED = 2
+
+def count_islands(grid):
+
+  memo = []
+
+  for _ in range(len(grid)):
+    row = []
+    for _ in range(len(grid[0])):
+      row.append(State.UNVISITED)
+    memo.append(row)
+
+  count = 0
+  
+  for i in range(len(grid)):
+    for j in range(len(grid[0])):
+      if grid[i][j] == '1' and memo[i][j] == State.UNVISITED :
+        count += 1
+        count_islands_helper(grid, i, j, memo)
+
+  return count
+
+
+def count_islands_helper(grid, i, j, memo):
+  if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
+    return
+
+  if grid[i][j] == '0':
+    return
+
+  if memo[i][j] == State.VISITING or memo[i][j] == State.VISITED:
+    return
+
+  memo[i][j] = State.VISITING
+
+  points = [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]
+
+  for point in points:
+    count_islands_helper(grid, point[0], point[1], memo)
+  
+  memo[i][j] = State.VISITED
+
+
+# grid = [
+#   ["0","0","0","0","0"],
+#   ["0","0","0","0","0"],
+#   ["0","0","1","0","0"],
+#   ["0","0","0","0","0"]
+# ]
+
+grid2 = [
+         ["1", "1", "0", "0", "0"], 
+         ["1", "1", "0", "0", "0"],
+         ["0", "0", "1", "0", "0"], 
+         ["0", "0", "0", "1", "1"]
+        ]
+
+print(count_islands(grid2))
